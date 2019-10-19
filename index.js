0d2259a1f0597b3565925ed74181b7bfeb8bc4ad
Ahmed Gamal <ahmedmgh67@gmail.com>
2019-10-19 20:30:21 +0200

    New landing and a lot of updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //hospital management
 var mongoose = require('mongoose');
-var express = require('express'),app = express(), port = process.env.PORT || 5000;
+var express = require('express'), app = express(), port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
 var uniqueValidator = require('mongoose-unique-validator');
 var axios = require('axios');
@@ -26,119 +26,123 @@ app.listen(
   )
 );
 //schema
-var TransictionSchema = new Schema({
-  user:{
+var TransactionSchema = new Schema({
+  sender: {
     type: String,
     required: true
   },
-  desc:{
+  rece: {
+    type: String,
+    required: true
+  },
+  desc: {
     type: String,
     required: false
   },
-  amount:{
+  amount: {
     type: String,
     required: true
   },
-  status:{
+  status: {
     type: String,
     default: "Waiting to Open"
   },
-  date:{
+  date: {
     default: Date.now(),
     type: Date
   }
 })
-mongoose.model("transictions", TransictionSchema);
-var Transiction = mongoose.model("transictions");
-listTransictions = function(req, res){
+mongoose.model("transactions", TransactionSchema);
+var Transaction = mongoose.model("transactions");
+listTransactions = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  Transiction.find({user: req.params.userId}, function(err, transictions) {
+  Transaction.find({ user: req.params.userId }, function (err, transactions) {
     if (err)
       res.send(err);
-    res.json(transictions);
+    res.json(transactions);
   });
 }
-searchTransictions = function(req, res){
+searchTransactions = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  Transiction.findOne({_id: req.params.id}, function(err, transictions) {
+  Transaction.findOne({ _id: req.params.id }, function (err, transactions) {
     if (err)
       res.send(err);
-    res.json(transictions);
+    res.json(transactions);
   });
 }
-listAllTransictions = function(_req, res){
+listAllTransactions = function (_req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  Transiction.find({}, function(err, transictions) {
+  Transaction.find({}, function (err, transactions) {
     if (err)
       res.send(err);
-    res.json(transictions);
+    res.json(transactions);
   });
 }
-createTransiction= function(req, res) {
+createTransaction = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   var newTransiction = new Transiction(req.body);
-  newTransiction.save(function(err, transiction) {
+  newTransiction.save(function (err, transaction) {
     if (err)
       res.send(err);
-    res.json(transiction);
+    res.json(transaction);
   });
 };
 
-updateTransiction = function(req, res) {
+updateTransaction = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  Transiction.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, transiction) {
+  Transaction.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, function (err, transaction) {
     if (err)
       res.send(err);
-    res.json(transiction);
+    res.json(transaction);
   });
 };
 
-deleteTransiction = function(req, res) {
+deleteTransaction = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   Post.remove({
     _id: req.params.id
-  }, function(err, _task) {
+  }, function (err, _task) {
     if (err)
       res.send(err);
-    res.json({ message: 'Transiction successfully deleted' });
+    res.json({ message: 'Transaction successfully deleted' });
   });
 };
 // handling the routes
-app.route('/api/transictions/:userId')
-  .get(listTransictions)
-app.route('/api/alltransictions')
-  .get(listAllTransictions)
-app.route('/api/searchtransictions/:id')
-  .get(searchTransictions)
-app.route('/api/transictions')
-  .post(createTransiction);
-app.route('/api/transictions/:id')
-  .put(updateTransiction)
-  .delete(deleteTransiction);
+app.route('/api/transactions/:userId')
+  .get(listTransactions)
+app.route('/api/alltransactions')
+  .get(listAllTransactions)
+app.route('/api/searchtransactions/:id')
+  .get(searchTransactions)
+app.route('/api/transactions')
+  .post(createTransaction);
+app.route('/api/transactions/:id')
+  .put(updateTransaction)
+  .delete(deleteTransaction);
 
 
-  
+
 //the user
 //schema
 var UserSchema = new Schema({
-  name:{
+  name: {
     type: String,
     required: true
   },
-  email:{
-    type: String,
-    required: true,
-    unique: true
-  },
-  password:{
+  // emaail: {
+  //   type: String,
+  //   required: true,
+  //   unique: true
+  // },
+  password: {
     type: String,
     required: true
   },
-  balance:{
+  balance: {
     type: String,
     default: "0"
   },
-  business:{
+  business: {
     type: Boolean,
     default: false
   },
@@ -148,58 +152,59 @@ var UserSchema = new Schema({
   // },
   phone: {
     type: String,
-    required: true
+    required: true,
+    unique:true
   }
 });
-UserSchema.plugin(uniqueValidator);
+// UserSchema.plugin(uniqueValidator);
 mongoose.model("users", UserSchema);
 var User = mongoose.model("users");
-login = function(req, res) {
+login = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  User.findOne({email: req.body.email, password: req.body.password}, function(err, requests) {
+  User.findOne({ phone: req.body.phone, password: req.body.password }, function (err, requests) {
     if (err)
       res.send(err);
     res.json(requests);
   });
 };
-balance = function(req, res){
+balance = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   //console.log(req.body.balance);
-  User.findByIdAndUpdate({_id: req.params.id}, {balance: req.params.balance}, function(err, raw){
+  User.findByIdAndUpdate({ _id: req.params.id }, { balance: req.params.balance }, function (err, raw) {
     if (err)
       res.send(err);
     res.json(raw);
   });
 };
-account = function(req, res) {
+account = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  User.findOne({email: req.params.email}, function(err, requests) {
+  User.findOne({ phone: req.params.phone }, function (err, requests) {
     if (err)
       res.send(err);
     res.json(requests);
   });
 };
-accounts = function(_req, res) {
+accounts = function (_req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  User.find({}, function(err, requests) {
+  User.find({}, function (err, requests) {
     if (err)
       res.send(err);
     res.json(requests);
   });
 };
-register = function(req, res) {
+register = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   var newUser = new User(req.body);
-  newUser.save(function(err, request) {
+  newUser.save(function (err, request) {
     if (err)
       res.send(err);
     res.json(request);
   });
 };
-app.route("/api/account/:email")
+app.route("/api/account/:phone")
   .get(account)
 app.route("/api/accounts/")
-.get(accounts)
+  .get(accounts)
 app.route("/api/login")
   .post(login)
 app.route("/api/register")
@@ -208,66 +213,66 @@ app.route("/api/balance/:id/:balance")
   .get(balance)
 
 var PaymentSchema = new Schema({
-  transiction:{
+  transiction: {
     type: String,
     required: true
   },
-  user:{
+  user: {
     type: String,
     required: true
   },
-  address:{
+  address: {
     type: String,
     required: false
   },
-  name:{
+  name: {
     type: String,
     required: true
   },
-  phone:{
+  phone: {
     type: String,
     required: true
   },
-  amount:{
+  amount: {
     type: String,
     required: true
   },
-  withdraw:{
-    type: Boolean, 
+  withdraw: {
+    type: Boolean,
     default: false,
   },
-  status:{
-    type:String,
-    default:"Waiting to Pay"
+  status: {
+    type: String,
+    default: "Waiting to Pay"
   }
 });
 mongoose.model("payments", PaymentSchema);
 var Payment = mongoose.model("payments");
 
-newPayment = function(req, res) {
+newPayment = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   var newPayment = new Payment(req.body);
-  newPayment.save(function(err, payment) {
+  newPayment.save(function (err, payment) {
     if (err)
       res.send(err);
     res.json(payment);
   });
 };
 
-listAllPayments = function(_req, res){
+listAllPayments = function (_req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  Payment.find({}, function(err, payments) {
+  Payment.find({}, function (err, payments) {
     if (err)
       res.send(err);
     res.json(payments);
   });
 }
 
-deletePayment = function(req, res) {
+deletePayment = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   Payment.remove({
     _id: req.params.id
-  }, function(err, _payment) {
+  }, function (err, _payment) {
     if (err)
       res.send(err);
     res.json({ message: 'Payment successfully deleted' });
@@ -277,69 +282,70 @@ deletePayment = function(req, res) {
 app.route("/api/payments")
   .post(newPayment);
 app.route("/api/payments/:id")
-  .delete(deletePayment); 
+  .delete(deletePayment);
 app.route("/api/allpayments")
   .get(listAllPayments);
 
-  app.route("/")
-    .get(function(_req, res){
-      res.sendFile(path.join(__dirname , "index/index.html"));   
-    });
+  app.use(express.static("indexx"));
+app.route("/")
+  .get(function (_req, res) {
+    res.sendFile(path.join(__dirname, "indexx/index.html"));
+  });
 app.use(express.static("pay"));
 mongoose.set('useFindAndModify', false);
 
-close = function(req, res){
+close = function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
-  Payment.findById(req.params.id, function(err,re){
-    if(err){
+  Payment.findById(req.params.id, function (err, re) {
+    if (err) {
       res.send(err);
     }
-    if(!re){
-      res.json({"error":"unavailablePayment"})
+    if (!re) {
+      res.json({ "error": "unavailablePayment" })
       return;
     }
-    if(re.transiction.indexOf("deposit")!=-1){
+    if (re.transiction.indexOf("deposit") != -1) {
       var a = re.transiction.slice(8);
-      User.findOne({email:a}, function(err,user){
-        if(err){
+      User.findOne({ phone: a }, function (err, user) {
+        if (err) {
           res.send(err);
         }
         var c = parseInt(user.balance) + parseInt(re.amount);
-        User.findOneAndUpdate({email: a}, {balance: c.toString()}, function(_err,_doc,resa){
+        User.findOneAndUpdate({ phone: a }, { balance: c.toString() }, function (_err, _doc, resa) {
           res.json(resa);
         });
       });
     }
-    if(re.transiction.indexOf("withdraw")!=-1){
+    if (re.transiction.indexOf("withdraw") != -1) {
       var b = re.transiction.slice(9);
-      User.findOne({email:b}, function(err,user){
-        if(err){
+      User.findOne({ phone: b }, function (err, user) {
+        if (err) {
           res.send(err);
         }
         var d = parseInt(user.balance) - parseInt(re.amount);
-        User.findOneAndUpdate({email: b}, {balance: d.toString()}, function(_err,_doc,resad){
+        User.findOneAndUpdate({ phone: b }, { balance: d.toString() }, function (_err, _doc, resad) {
           res.json(resad);
         });
       });
     } else {
-      User.findOne({email:re.user}, function(err,user){
-        if(err){
+      User.findOne({ phone: re.user }, function (err, user) {
+        if (err) {
           res.send(err);
         }
-        
+
         console.log(user);
         var c = (parseInt(user.balance) + parseInt(re.amount)) * 0.97;
-        User.findOneAndUpdate({email: a}, {balance: c.toString()}, function(err,_doc,resa){
-          if(err){
+        User.findOneAndUpdate({ phone: a }, { balance: c.toString() }, function (err, _doc, resa) {
+          if (err) {
             res.send(err);
           }
           res.json(resa);
         });
       });
     }
-    
+
   });
-  Payment.deleteOne({_id: req.params.id}, function(err, _resd){
+  Payment.deleteOne({ _id: req.params.id }, function (err, _resd) {
     if (err)
       res.send(err)
   });
@@ -349,19 +355,20 @@ app.route("/api/close/:id")
   .get(close)
 
 
-qrcode = function (req, res){
-  User.findOne({email:req.params.charged}, function(_err, dbresc){
-    if(Number.parseInt(req.params.amount) > Number.parseInt(dbresc.balance)){
-      res.json({"message":"amount is less than balance"});
+qrcode = function (req, res) {
+  User.findOne({ phone: req.params.sender }, function (_err, dbresc) {
+    if (Number.parseInt(req.params.amount) > Number.parseInt(dbresc.balance)) {
+      res.json({ "message": "amount is less than balance" });
     } else {
-      User.findOneAndUpdate({email:req.params.charged}, {balance: (Number.parseInt(dbresc.balance) - Number.parseInt(req.params.amount)).toString()}, function(_err, _resa){})
-      User.findOne({email: req.params.email}, function(_err, emaildbres){
-        User.findOneAndUpdate({email:req.params.email}, {balance: (Number.parseInt(emaildbres.balance) + Number.parseInt(req.params.amount)).toString()}, function(_err, _resab){})
+      User.findOneAndUpdate({ phone: req.params.sender }, { balance: (Number.parseInt(dbresc.balance) - Number.parseInt(req.params.amount)).toString() }, function (_err, _resa) { })
+      User.findOne({ phone: req.params.rece }, function (_err, phonedbres) {
+        User.findOneAndUpdate({ phone: req.params.rece }, { balance: (Number.parseInt(phonedbres.balance) + Number.parseInt(req.params.amount)).toString() }, function (_err, _resab) { })
       })
-      res.json({"message":"operation complete"})
+
+      res.json({ "message": "operation complete" })
     }
   });
 }
 
-app.route("/api/qrcode/:email/:charged/:amount")
+app.route("/api/qrcode/:sender/:rece/:amount")
   .get(qrcode)
